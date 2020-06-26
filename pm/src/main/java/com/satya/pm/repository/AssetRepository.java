a3c0db62e2e994b7f73ea6f97aa862b23dd5adf2
satya <satyanarayana.mv@gmail.com>
2020-06-26 10:48:02 +0530

    Cleanup

--- pm/src/main/java/com/satya/pm/repository/AssetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.satya.pm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.satya.pm.repository.model.Asset;
-
-public interface AssetRepository extends JpaRepository<Asset, Long> {
-
-}
